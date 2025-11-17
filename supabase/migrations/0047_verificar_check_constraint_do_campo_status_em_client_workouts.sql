a0b53f603dbee72e378baf891f747fb632da4f35
[dyad] <giulianomsg@gmail.com>
2025-11-17 05:29:20 -0300

    [dyad] wrote 3 file(s), executed 1 SQL queries

---
+++ supabase/migrations/0047_verificar_check_constraint_do_campo_status_em_client_workouts.sql
@@ -0,0 +1,7 @@
+SELECT 
+  conname as constraint_name,
+  pg_get_constraintdef(oid) as constraint_definition
+FROM pg_constraint 
+WHERE conrelid = 'public.client_workouts'::regclass 
+  AND contype = 'c'
+  AND conname LIKE '%status%';
